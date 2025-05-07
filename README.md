ff72457f31a8d147e621e93368279978cc680c94
michaeljav <michaeljaviermota@gmail.com>
2025-05-07 18:39:12 -0400

    ya recupera  el chat y guarda

--- README.md
+++ README.md
@@ -1,5 +1,51 @@
 # chat_node
 
-#morgan : es un logger que funciona a nivel de request para express. herramienta que Para saber como esta funcionando nuestro servidor
--si algo esta fallando
--duracion de la respuesta de un servidor
+## Morgan
+
+[Morgan](https://www.npmjs.com/package/morgan) is an HTTP logger middleware for **Node.js** and **Express**.
+
+- Logs incoming requests to the server.
+- Useful for debugging, monitoring, and measuring performance.
+- Commonly used in development and testing.
+
+📦 _Official NPM package_: `morgan`
+
+## Turso
+
+[Turso](https://turso.tech) is a **fast, lightweight, and distributed** SQL database built on SQLite.
+
+- Designed to run close to the user (edge computing).
+- Ideal for modern apps with low latency.
+- SQLite-compatible and easy to use from both frontend and backend.
+
+📦 _Official NPM client_: `@turso/client`
+
+install Turso CLI in windows wsl:
+curl -sSfL https://get.tur.so/install.sh | bash
+
+## cmd: turso db create
+
+Created group default at aws-us-east-1 in 719ms.
+Created database saving-constrictor at group default in 1.211s.
+
+Start an interactive SQL shell with:
+
+turso db shell saving-constrictor
+
+To see information about the database, including a connection URL, run:
+
+turso db show saving-constrictor
+
+To get an authentication token for the database, run:
+
+turso db tokens create saving-constrictor
+
+## 📦 Dependency for Working with Turso
+
+To connect your application with **Turso**, you need to install the official LibSQL client and the `dotenv` library to manage environment variables.
+
+### Installation
+
+```bash
+npm install @libsql/client dotenv
+```
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import logger from 'morgan';
+import dotenv from 'dotenv';
+import { createClient } from '@libsql/client';
 
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
 
+dotenv.config();
 const port = process.env.PORT ?? 3000;
 
 const app = express();
@@ -11,9 +14,26 @@ const app = express();
 //agregamos nuestra app de express a un servidor http
 const server = createServer(app);
 //y tenemos los websockets en el mismo servidor
-const io = new Server(server);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
 
-io.on('connection', (socket) => {
+// console.log('url ', process.env.DB_URL);
+// console.log('token ', process.env.DB_TOKEN);
+// console.log('+++++++++++++++++++++++++++++++++++');
+const db = createClient({
+  url: process.env.DB_URL,
+  authToken: process.env.DB_TOKEN,
+});
+
+await db.execute(`CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  content TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)`);
+
+io.on('connection', async (socket) => {
+  console.log('Auth data from client:', socket.handshake.auth);
   console.log('a user has connected');
 
   socket.on('disconnect', () => {
@@ -21,10 +41,42 @@ io.on('connection', (socket) => {
   });
 
   //escuchamos el evento de chat message y lo mostramos en consola
-  socket.on('chat message', (msg) => {
+  socket.on('chat message', async (msg) => {
     console.log('message: ' + msg);
+    let result;
+    try {
+      result = await db.execute({
+        sql: `INSERT INTO messages (content) VALUES (:msg)`,
+        args: { msg },
+      });
+    } catch (error) {
+      console.error('Error inserting message:', error);
+      return;
+    }
+    //emit to all clients
+    io.emit('chat message', msg, result.lastInsertRowid.toString()); //enviamos el mensaje a todos los clientes conectados
     //enviamos el mensaje a todos los clientes conectados
   });
+
+  // console.log('info of socket');
+  // console.log(socket.handshake);
+  // console.log(socket.handshake.auth);
+
+  if (!socket.recovered) {
+    //recuperar los mensajes  sin conexion
+    try {
+      const result = await db.execute({
+        sql: 'SELECT id, content, created_at FROM messages where id > ? ',
+        args: [socket.handshake.auth.serverOffset ?? 0],
+      });
+
+      result.rows.forEach((row) => {
+        socket.emit('chat message', row.content, row.id.toString()); //enviamos el mensaje a todos los clientes conectados
+      });
+    } catch (error) {
+      console.error('Error retrieving messages:', error);
+    }
+  }
 });
 
 //usarlo en modo de desarrollo
